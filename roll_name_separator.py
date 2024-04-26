583feef766d6b62e4c11e06092919bd130b7e3bd
Thejus R. Kartha <33420541+tjkartha@users.noreply.github.com>
2024-04-26 17:31:55 +0530

    For separating Roll Numbers from names
    The ERP has an option that lets you download the attendance sheet in a .csv format. However, the Roll numbers and names of the students all appear in a single cell, and cannot be separated easily. So, this Python script exactly does that. The output is a csv file containing the roll numbers and names separated, and only that. The attendance information is not preserved.
    Input : export.csv
    Output: Rolls_names.csv

---
+++ roll_name_separator.py
@@ -0,0 +1,15 @@
+import pandas as pd
+a = pd.read_csv("export.csv")
+
+a = a.iloc[1:, 1]
+
+names = []
+rolls = []
+for x in a:
+    rolls.append(x[:16])
+    names.append(x[18:])
+
+rolls_names = list(zip(rolls, names))
+rolls_names_df = pd.DataFrame(rolls_names)
+rolls_names_df.to_csv("Rolls_names.csv")
+
